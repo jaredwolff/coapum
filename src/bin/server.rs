081a1db4d74cbca45d2576e5670400ed3396716b
Jared Wolff <hello@jaredwolff.com>
2024-03-31 13:12:10 -0400

    Example of how to using a global PSK store for updating devices on the fly

--- src/bin/server.rs
+++ src/bin/server.rs
@@ -1,4 +1,8 @@
-use std::{self, sync::Arc};
+use std::{
+    self,
+    collections::HashMap,
+    sync::{Arc, RwLock},
+};
 
 use coapum::{
     dtls::{
@@ -11,6 +15,8 @@ use coapum::{
     router::{wrapper::get, CoapRouter, Request},
     serve, ResponseType,
 };
+
+type PskStore = Arc<RwLock<HashMap<String, Vec<u8>>>>;
 
 const PSK: &[u8] = "63ef2024b1de6417f856fab7005d38f6".as_bytes();
 
@@ -29,6 +35,15 @@ async fn main() {
 
     log::info!("Server!");
 
+    // Set up store
+    let psk_store: PskStore = Arc::new(RwLock::new(HashMap::new()));
+
+    // Add entry
+    {
+        let mut psk_store_write = psk_store.write().unwrap();
+        psk_store_write.insert("goobie!".to_string(), PSK.to_vec());
+    }
+
     let obs = SledObserver::new("coapum.db");
 
     let mut router = CoapRouter::new((), obs);
@@ -37,13 +52,18 @@ async fn main() {
     // Setup socket
     let addr = "127.0.0.1:5684";
     let dtls_cfg = Config {
-        psk: Some(Arc::new(|hint: &[u8]| -> Result<Vec<u8>, Error> {
-            log::info!(
-                "Client's hint: {}",
-                String::from_utf8(hint.to_vec()).unwrap()
-            );
+        psk: Some(Arc::new(move |hint: &[u8]| -> Result<Vec<u8>, Error> {
+            let hint = String::from_utf8(hint.to_vec()).unwrap();
 
-            Ok(PSK.to_vec())
+            log::info!("Client's hint: {}", hint);
+
+            // Look up the hint in the database
+            if let Some(psk) = psk_store.read().unwrap().get(&hint) {
+                return Ok(psk.clone());
+            } else {
+                log::info!("Hint {} not found in store", hint);
+                Err(Error::ErrIdentityNoPsk)
+            }
         })),
         psk_identity_hint: Some("coapum server".as_bytes().to_vec()),
         cipher_suites: vec![CipherSuiteId::Tls_Psk_With_Aes_128_Gcm_Sha256],
