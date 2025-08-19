cc56ce20ff9d1db26b429820ff8b47a4fda8760f
github-action-benchmark <github@users.noreply.github.com>
2025-08-19 22:46:05 +0000

    add coapum Criterion (cargo) benchmark result for 2a0c10153da053b1e84a9e318f5dfa3eebfdffca

---
+++ dev/bench/data.js
@@ -0,0 +1,38 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1755643565811,
+  "repoUrl": "https://github.com/jaredwolff/coapum",
+  "entries": {
+    "coapum Criterion": [
+      {
+        "commit": {
+          "author": {
+            "email": "hello@jaredwolff.com",
+            "name": "Jared Wolff",
+            "username": "jaredwolff"
+          },
+          "committer": {
+            "email": "hello@jaredwolff.com",
+            "name": "Jared Wolff",
+            "username": "jaredwolff"
+          },
+          "distinct": true,
+          "id": "2a0c10153da053b1e84a9e318f5dfa3eebfdffca",
+          "message": "fix: Fix benchmark output format and storage for CI workflow",
+          "timestamp": "2025-08-19T18:38:59-04:00",
+          "tree_id": "e9656a38c5824df123b4094771c78b6c0a2c9116",
+          "url": "https://github.com/jaredwolff/coapum/commit/2a0c10153da053b1e84a9e318f5dfa3eebfdffca"
+        },
+        "date": 1755643565554,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "coap_router",
+            "value": 1125,
+            "range": "± 7",
+            "unit": "ns/iter"
+          }
+        ]
+      }
+    ]
+  }
+}
