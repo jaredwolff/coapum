0676fa740caf5523570f4f3f76757a457fb360b7
github-action-benchmark <github@users.noreply.github.com>
2025-08-20 01:19:54 +0000

    add coapum Criterion (cargo) benchmark result for d0829adb090de88799a4e7b3af1a3fa69ec87940

---
+++ data.js
@@ -0,0 +1,38 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1755652794930,
+  "repoUrl": "https://github.com/jaredwolff/coapum",
+  "entries": {
+    "coapum Criterion": [
+      {
+        "commit": {
+          "author": {
+            "email": "hello@jaredwolff.com",
+            "name": "Jared Wolff",
+            "username": "jaredwolff"
+          },
+          "committer": {
+            "email": "hello@jaredwolff.com",
+            "name": "Jared Wolff",
+            "username": "jaredwolff"
+          },
+          "distinct": true,
+          "id": "d0829adb090de88799a4e7b3af1a3fa69ec87940",
+          "message": "fix: saving bench output to root on gh-pages",
+          "timestamp": "2025-08-19T21:17:54-04:00",
+          "tree_id": "c2e0bd07a2b66cb5ad08eddd99378d45c4b1cf5b",
+          "url": "https://github.com/jaredwolff/coapum/commit/d0829adb090de88799a4e7b3af1a3fa69ec87940"
+        },
+        "date": 1755652793924,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "coap_router",
+            "value": 1147,
+            "range": "± 18",
+            "unit": "ns/iter"
+          }
+        ]
+      }
+    ]
+  }
+}
